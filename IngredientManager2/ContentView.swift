da8955030898ea40dcad0c7f868a20e6c353ab09
weebayModz <weebaymodz@gmail.com>
2020-12-19 14:39:39 -0500

    Initial Commit

---
+++ IngredientManager2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IngredientManager2
+//
+//  Created by Dominick Vaughn on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IngredientManager2/IngredientManager2App.swift
@@ -0,0 +1,17 @@
+//
+//  IngredientManager2App.swift
+//  IngredientManager2
+//
+//  Created by Dominick Vaughn on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct IngredientManager2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
