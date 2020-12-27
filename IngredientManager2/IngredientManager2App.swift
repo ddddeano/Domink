d7ec6c5349f4a43c41e5c00d86b813acb6f23766
weebayModz <weebaymodz@gmail.com>
2020-12-27 12:13:18 -0500

    redoing the commit

--- IngredientManager2/IngredientManager2App.swift
+++ IngredientManager2/IngredientManager2App.swift
@@ -5,13 +5,40 @@
 //  Created by Dominick Vaughn on 12/19/20.
 //
 
+
 import SwiftUI
+import CoreData
 
 @main
 struct IngredientManager2App: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, pC.viewContext)
+        }
+    }
+}
+
+var pC: NSPersistentContainer = {
+    
+    let container = NSPersistentContainer(name: "Model" )
+    container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        if let error = error as NSError? {
+            fatalError( "unresolved error \(error), \(error.userInfo)")
+        }
+    })
+    return container
+}()
+func saveContext () {
+    let context = pC.viewContext
+    if context.hasChanges {
+        do {
+            try context.save()
+        } catch {
+            
+            let nserror = error as NSError
+            fatalError( " unresolved error \(nserror), \(nserror.userInfo)")
         }
     }
 }
